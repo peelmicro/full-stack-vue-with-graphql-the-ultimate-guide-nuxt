0333090d723e154eefac9845f3b50896bf06a98d
peelmicro <juanp_perez@msn.com>
2019-08-19 18:09:53 +0100

    Heroku Deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
+import * as express from 'express';
 import { NestFactory } from '@nestjs/core';
+import { join }  from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (process.env.NODE_ENV === 'production') {
+    const CLIENT_FILES = join(__dirname, '..', '..', 'client', 'dist');
+    app.use(express.static(CLIENT_FILES));
+  }
   await app.listen(process.env.PORT || 4000);
 }
-bootstrap();
+bootstrap(); 
